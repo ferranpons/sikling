e38d8a60bf4900f937caf14efbc3340a10c4d2e4
Ferran Pons <ferran.pons@scmspain.com>
2018-10-10 17:48:50 +0200

    removed unused imports

--- app/src/main/java/com/sikling/app/MainActivity.kt
+++ app/src/main/java/com/sikling/app/MainActivity.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.support.design.internal.BottomNavigationMenuView
-import android.support.design.internal.BottomNavigationItemView
 import com.sikling.app.adInsertion.AdInsertionFragment
 import com.sikling.app.adListing.AdListingFragment
 import com.sikling.app.favorites.FavoritesFragment

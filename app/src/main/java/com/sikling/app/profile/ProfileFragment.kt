6367ab18e6e87694b7f12ddf12700a34aaf5a4ef
Ferran Pons <ferranpons@gmail.com>
2019-10-01 14:27:22 +0200

    solve some deprecations

--- app/src/main/java/com/sikling/app/profile/ProfileFragment.kt
+++ app/src/main/java/com/sikling/app/profile/ProfileFragment.kt
@@ -1,15 +1,15 @@
 package com.sikling.app.profile
 
-import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.sikling.app.R
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
